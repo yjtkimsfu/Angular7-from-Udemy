d71cbca1b07fef741f8523a90244a9e4789ac565
YJ Tristan Kim <yjtkimsfu@gmail.com>
2019-04-10 13:10:03 -0700

    Section 21: Adding feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { RecipesComponent } from './recipes/recipes.component';
+
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuard } from './_services/auth-guard.service';
 
 const appRoute: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, children:
-    [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
-    ]
-  },
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'signup', component: SignupComponent},
-  { path: 'signin', component: SigninComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoute)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './_directives/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListService } from './_services/shopping-list.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './_services/recipe.service';
 import { DataStorageService } from './_services/data-storage.service';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthService } from './_services/auth.service';
 import { AuthGuard } from './_services/auth-guard.service';
+import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModuel } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
    declarations: [
       AppComponent,
       HeaderComponent,
-      RecipesComponent,
-      RecipeListComponent,
-      RecipeDetailComponent,
-      RecipeItemComponent,
-      RecipeStartComponent,
-      RecipeEditComponent,
-      ShoppingListComponent,
-      ShoppingEditComponent,
-      DropdownDirective,
-      SigninComponent,
-      SignupComponent
    ],
    imports: [
       BrowserModule,
-      FormsModule,
-      ReactiveFormsModule,
       HttpModule,
+      RecipesModule,
+      SharedModule,
+      ShoppingListModuel,
+      AuthModule,
       AppRoutingModule
    ],
    providers: [
